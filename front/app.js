fdccc9511ca570935fd1e243ec6409cf13558734
glamagonzalez <gonzalez.glama25@gmail.com>
2025-06-23 19:55:51 -0300

    first commit

--- front/app.js
+++ front/app.js
@@ -1,6 +1,6 @@
 const API_URL = "http://localhost:5001/api/students";
 const API_KEY = "12345ABCDEF";
-
+// super chino//
 // Headers comunes para todas las peticiones
 const headers = {
     "Content-Type": "application/json",
